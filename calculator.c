a64ed44a3fb6a97d9a926c94594d2c5db10d45d9
Domonkos Tóth <62752558+domyoyo@users.noreply.github.com>
2022-09-18 20:57:08 +0200

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+    int a, b;
+    char op;
+
+printf("Adj meg egy szamot: ");
+scanf("%d" , &a);
+printf("a = %d\n" , a);
+printf("Adj meg egy szamot: ");
+scanf("%d" , &b);
+printf("b = %d" , b);
+printf("\nAdj meg egy muveletet: ");
+scanf(" %c" , &op);
+printf("c = %c" , op);
+printf("Eredmeny: ", a);
+
+ if (op == '/')
+   printf("\nEredmeny: %d", a/b);
+ else
+   printf("\nNem definialt muvelet");
+}   
+
+
+ 
